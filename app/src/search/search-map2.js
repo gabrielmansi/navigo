6297f6f03fbee16f7fd546cab25626eabded2dde
scottlepp <scott.lepper@gmail.com>
2016-06-22 07:39:04 -0400

    VG-4245 - don't zoom out ignoring configured extent and zoom
    # Conflicts:
    #	app/src/search/search-map2.js

--- app/src/search/search-map2.js
+++ app/src/search/search-map2.js
@@ -1,7 +1,7 @@
 /*global angular, $, L */
 
 angular.module('voyager.search')
-	.directive('vsSearchMap', function ($compile, config, mapUtil, $timeout, mapControls, configService, $window, $http, sugar, $rootScope, mapCustomControls) {
+	.directive('vsSearchMap', function ($compile, config, mapUtil, $timeout, mapControls, configService, $window, $http, sugar, $rootScope, mapCustomControls, searchViewService) {
 		'use strict';
 
 		function getMapSizeTemplate() {
@@ -547,6 +547,7 @@ angular.module('voyager.search')
 				$scope.$watch('view', function(view){
 					_cancelDraw();
 					leafletData.getMap('search-map').then(function (map) {
+						// TODO - limit zoom on table view?  Map is big there too
 						if(view === 'map') {
 							map.options.minZoom = 2;  //keep from zooming out too far when the map is big
 						} else {
@@ -556,7 +557,7 @@ angular.module('voyager.search')
 						$timeout(function () {  //workaround for leaflet bug:  https://github.com/Leaflet/Leaflet/issues/2021
 							map.invalidateSize();  //workaround when initially hidden
 							// console.log('view change - moving');
-							if(!angular.isDefined(map.currentBounds)) {
+							if(angular.isUndefined(map.currentBounds) && angular.isUndefined(searchViewService.getUserView()) && $attrs.origin !== 'home') {
 								$scope.resizeMap();
 							}
 							// adjust layers control placement if table view is showing map size toggle control
@@ -630,7 +631,9 @@ angular.module('voyager.search')
 
 								var emptySpace = Math.ceil(ratio - Math.floor(ratio)) + 1;
 								if (emptySpace > 0) {
-									map.setZoom(emptySpace);
+									// TODO - zooming to try and fill in empty space?
+									// this is problematic since the config defines the extent and zoom and this will ignore that
+									//map.setZoom(emptySpace);
 								}
 
 								if (isFull) {
