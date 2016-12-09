8fa7e5b60a241738b7b65d6b9b70e2f2a9c215ba
john-atkinson-oci <john.atkinson@owlcomputing.com>
2016-12-09 18:30:02 -0500

    VG-1581 - Resolved some requested changes and completed unit tests

---
+++ test/spec/modal/confirm-modal-controller-spec.js
@@ -0,0 +1,59 @@
+'use strict';
+
+describe('Controller: ConfirmModalCtrl', function () {
+
+    var $scope, $controller, $uibModalInstance, sut;
+    var cfg = _.clone(config);
+
+    beforeEach(function () {
+        module('voyager.modal');
+        module('ui.bootstrap');
+        module('voyager.config');
+        module(function ($provide) {
+            $provide.constant('config', cfg);
+        });
+
+        inject(function (_$controller_, $rootScope) {
+            $scope = $rootScope.$new();
+            $uibModalInstance = {
+                close: function() {},
+                dismiss: function() {}
+            };
+            $controller = _$controller_;
+        });
+    });
+
+    // Specs here
+
+    function initController() {
+        sut = $controller('ConfirmModalCtrl', {$scope: $scope, $uibModalInstance: $uibModalInstance});
+    }
+
+    it('should instantiate', function () {
+        initController();
+        expect(sut).not.toBeUndefined();
+    });
+
+    it('should return default statuses', function () {
+        initController();
+
+        expect(sut.notStarted).toBe(true);
+        expect(sut.isWaiting).toBe(false);
+        expect(sut.hasCompleted).toBe(false);
+        expect(sut.hasError).toBe(false);
+    });
+
+    it('should confirm', function () {
+        initController();
+        spyOn($uibModalInstance, 'close');
+        sut.confirm();
+        expect($uibModalInstance.close).toHaveBeenCalledWith('Confirm');
+    });
+
+    it('should cancel', function () {
+        initController();
+        spyOn($uibModalInstance, 'dismiss');
+        sut.cancel();
+        expect($uibModalInstance.dismiss).toHaveBeenCalledWith('Cancel');
+    });
+});
