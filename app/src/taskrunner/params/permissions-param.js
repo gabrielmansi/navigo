6a687d5368a81ca25e7b2fd3c6c285f10c1e505d
jason <jason@voyagersearch.com>
2017-02-15 09:03:30 -0800

    VG-5103: New task parameter for permissions

---
+++ app/src/taskrunner/params/permissions-param.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('taskRunner')
+    .directive('vsPermissionsParam', function (authService) {
+
+        return {
+            replace: true,
+            scope: {
+                param: '=',
+                show: '='
+            },
+            template: '<input type="hidden" ui-select2="permissionOptions" ng-model="param.value" style="width: 100%;">',
+            controller: function ($scope, $element, $attrs) {
+
+                var _shareGroups = [];
+                var _coreRoles = [{id:'_EVERYONE',text:'EVERYONE'},{id:'_LOGGEDIN',text:'LOGGEDIN'},{id:'_ANONYMOUS',text:'ANONYMOUS'}];
+
+                var isMultiple = $attrs.multi !== 'false';
+
+                $scope.permissionOptions = {
+                    'multiple': isMultiple,
+                    'simple_tags': true,
+                    dropdownAutoWidth: true,
+                    data: function() {
+                        return {results:_shareGroups};
+                    }
+                };
+
+                authService.fetchGroups().then(function(groups) {
+                    $.merge(_shareGroups, groups);
+                    $.merge(_shareGroups, _coreRoles);
+                });
+            }
+        };
+    });
